0be4f7d5f84734ea0b3e761ac2f9da1788b0c038
Micka <mickael.votte@hotmail.com>
2022-07-29 15:22:38 +0200

    trois cards plus photo

--- view/home.php
+++ view/home.php
@@ -4,35 +4,66 @@
 <body>
 
 
-<div class="banner-image w-100 d-flex justify-content-center align-items-center">
-<div class="text-center">
-      <h1 class="text-white">Entre Nous</h1>
-  </div>  
-</div>
+    <div class="banner-image w-100 d-flex justify-content-center align-items-center">
+        <div class="text-center">
+            <h1 class="text-white">Entre Nous</h1>
+        </div>
+    </div>
 
 
-<hr>
+    <hr>
 
 
-<div class="container  p-5 blockImgText">
+    <div class="container1">
 
 
-<div class="row d-flex justify-content">
+        <div class="row d-flex justify-content m-0 p-0">
 
-<div class="col-lg-6 col-sm-12 textHome1">
-    <p class="blockHome1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
-</div>
+            <div class="col-lg-6 col-sm-12  textHome1">
+                <div class="blockHome1">
+                    <H2 class="pt-2 pb-2">Qui sommes nous?</H2>
+                    <p class=>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-12 imgHome1">
+                <img class="imgHome  " src="../assets/img/runninggirl.jpg" alt="persone-qui-court">
+            </div>
 
-<div class="col-lg-6 col-sm-12 imgHome1">
-    <img class="imgHome  " src="../assets/img/runninggirl.jpg" alt="persone-qui-court">
-</div>
-
-</div>
+        </div>
 
 
 
+        <div class="row d-flex justify-content-center m-5 p-0">
 
-</div>
+
+            <div class="col-lg-4 col-12 threecards">
+                <div class="p-"> 
+                    <img class="cardImg" src="../assets/img/firstcard.jpg" alt="">
+
+                    <H2 class="pt-2 pb-2">Entre Aide</H2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-12  threecards">
+                <div class=""> 
+                    <img class="cardImg" src="../assets/img/secondcard.jpg" alt="event-running">
+                    <H2 class="pt-2 pb-2">Evenement</H2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-12 threecards">
+                <div class=""> 
+                    <img class="cardImg" src="../assets/img/threecard.jpg" alt="">
+                    <H2 class="pt-2 pb-2">Communautaire</H2>
+                    <p class=>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </div>
 </body>
 
 
