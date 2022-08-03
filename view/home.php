4d8751ef20626c4a7be6e86aacc14b567a6662bf
Micka <mickael.votte@hotmail.com>
2022-08-03 23:20:33 +0200

    avancement de la page home + bouton

--- view/home.php
+++ view/home.php
@@ -1,7 +1,7 @@
 <?php include('../elements/top.php')   ?>
-<?php include('../elements/header.php')   ?>
 
 <body>
+    <?php include('../elements/header.php')   ?>
 
 
     <div class="banner-image w-100 d-flex justify-content-center align-items-center">
@@ -33,29 +33,32 @@
 
 
 
-        <div class="row d-flex justify-content-center m-5 p-0">
+        <div class="blockcards row d-flex justify-content-center p-0">
 
 
             <div class="col-lg-4 col-12 threecards">
-                <div class="p-"> 
+                <div class="p-">
                     <img class="cardImg" src="../assets/img/firstcard.jpg" alt="">
 
-                    <H2 class="pt-2 pb-2">Entre Aide</H2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
+                    <H2 class="pt-3 pb-3 ps-2 fw-bold">Entre Aide</H2>
+                    <p class="textcard">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! <span class="dots">....</span> <span class="moreText">Est provident atque sed vel quaerat laudantium quia explicabo! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium, explicabo.</span> </p>
+                    <button class="read-more-btn">
+                    Plus d'infos
+                    </button>
                 </div>
             </div>
             <div class="col-lg-4 col-12  threecards">
-                <div class=""> 
+                <div class="">
                     <img class="cardImg" src="../assets/img/secondcard.jpg" alt="event-running">
-                    <H2 class="pt-2 pb-2">Evenement</H2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
+                    <H2 class="pt-3 pb-3 ps-2 fw-bold">Evenement</H2>
+                    <p class="textcard">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
                 </div>
             </div>
             <div class="col-lg-4 col-12 threecards">
-                <div class=""> 
+                <div class="">
                     <img class="cardImg" src="../assets/img/threecard.jpg" alt="">
-                    <H2 class="pt-2 pb-2">Communautaire</H2>
-                    <p class=>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
+                    <H2 class="pt-3 pb-3 ps-2 fw-bold">Communautaire</H2>
+                    <p class='textcard'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, ratione? Aperiam eligendi voluptatem, corrupti quod enim quidem asperiores dolores perferendis quibusdam! Est provident atque sed vel quaerat laudantium quia explicabo!</p>
                 </div>
             </div>
 
@@ -64,10 +67,35 @@
 
 
     </div>
+
+
+
+    <div class="container2">
+        <div class="row d-flex justify-content-center pt-5 pb-5 p-0 m-0">
+
+            <div class=" blockBasPage col-lg-4 col-12">
+                <h2 class="rejNous text-center ">REJOIGNEZ <br> NOUS .</h2>
+            </div>
+
+            <div class=" blockBasPage col-lg-4 col-12 ">
+                <p class="ms-3 fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui iste corporis distinctio autem dolorem eos sed similique maiores quisquam quaerat.</p> 
+                <div class=" buttonBasPage ">
+                <a href="" class="btn btn1"> en savoir plus</a>
+            </div>
+            </div>
+
+           
+
+        </div>
+
+
+
+    </div>
+
+    <?php include('../elements/footer.php') ?>
 </body>
 
 
 
 
 
-<?php include('../elements/footer.php') ?>
